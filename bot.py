3f4ccd09f330c5cfeb670a7e159ba96ec8da5552
TRAKOS CSI <69827418+mhdeiiking@users.noreply.github.com>
2022-09-14 08:55:44 -0700

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,28 +1,8 @@
 import telebot
 
-from telebot.types import InlineKeyboardMarkup, InlineKeyboardButton
-
-TOKEN = "5448584711:AAEJ_LUBZF-6HaWoLOkxMB0motL5h5fkDo8"
-
-bot = telebot.TeleBot(TOKEN)
+bot = telebot.TeleBot("5448584711:AAEDCP1Glg7fjIhBWnaROKsKBWQHY-nJWGw")
 
 @bot.message_handler(commands=['start'])
-
 def start(message):
-
-    x = InlineKeyboardMarkup()
-
-    a = InlineKeyboardButton("Dev", url="t.me/zdddu")
-
-    b = InlineKeyboardButton("Channel", url="t.me/y88f8")
-
-    x.add(a,b)
-
-    bot.reply_to(message, "test", reply_markup=x)
-
-    
-
-print("[√] Your client has been started successfully")
-
+    bot.reply_to(message,f"- هلاو")
 bot.infinity_polling()
-
